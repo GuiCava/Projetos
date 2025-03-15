8f747728759cf189efef997bce0746557a42c077
GuiCava <166819622+GuiCava@users.noreply.github.com>
2025-03-15 18:34:34 -0300

    atualizado

--- projetoSenai/script.js
+++ projetoSenai/script.js
@@ -1,22 +1,26 @@
 function initTabNav() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section');
-  
-    if(tabMenu.length && tabContent.length) {
+  const tabMenu = document.querySelectorAll('.js-menu button');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  const imagens = document.querySelectorAll('.js-imagens img');
+
+  if (tabMenu.length && tabContent.length && imagens.length) {
       tabContent[0].classList.add('ativo');
-  
+      imagens[0].classList.add('ativo');
+
       function activeTab(index) {
-        tabContent.forEach((section) => {
-          section.classList.remove('ativo');
-        });
-        tabContent[index].classList.add('ativo');
+          tabContent.forEach((section) => section.classList.remove('ativo'));
+          imagens.forEach((img) => img.classList.remove('ativo'));
+
+          tabContent[index].classList.add('ativo');
+          imagens[index].classList.add('ativo');
       }
-  
+
       tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-          activeTab(index);
-        });
+          itemMenu.addEventListener('click', () => {
+              activeTab(index);
+          });
       });
-    }
   }
-  initTabNav();
+}
+
+initTabNav();
